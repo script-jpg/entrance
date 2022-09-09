fc13bfc0038b0d13cba0b826d89ec945a7c6e2b2
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2022-09-08 23:51:16 -0400

    Remove Nebular progress bar

--- src/app/components/creator/footer-content/setup-buy-call/setup-buy-call.component.ts
+++ src/app/components/creator/footer-content/setup-buy-call/setup-buy-call.component.ts
@@ -14,20 +14,4 @@ export class SetupBuyCallComponent implements OnInit {
 
   value = 25;
 
-  setValue(newValue: number) {
-    this.value = Math.min(Math.max(newValue, 0), 100)
-  }
-
-  get status() {
-    if (this.value <= 25) {
-      return 'danger';
-    } else if (this.value <= 50) {
-      return 'warning';
-    } else if (this.value <= 75) {
-      return 'info';
-    } else {
-      return 'success';
-    }
-  }
-
 }
