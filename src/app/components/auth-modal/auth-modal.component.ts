07344e3172ee9788b9fefded4b5f96266f55a901
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2022-09-27 19:24:12 -0400

    Revert "add googleAuth service"
    This reverts commit 412bf3f5287d3764dbdfa65644910104b532e232.

--- src/app/components/auth-modal/auth-modal.component.ts
+++ src/app/components/auth-modal/auth-modal.component.ts
@@ -3,7 +3,6 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 import { GoogleApiService } from 'src/app/services/google-api.service';
 import {OAuthService} from 'angular-oauth2-oidc';
 import { HttpClient } from '@angular/common/http';
-import { GoogleAuthService } from 'src/app/services/google-auth.service';
 
 @Component({
   selector: 'app-auth-modal',
@@ -12,10 +11,9 @@ import { GoogleAuthService } from 'src/app/services/google-auth.service';
 })
 export class AuthModalComponent implements OnInit {
   googleApi: GoogleApiService;
-  googleAuth: GoogleAuthService;
-  constructor(googleApi: GoogleApiService, googleAuth: GoogleAuthService) {
+
+  constructor(googleApi: GoogleApiService) {
     this.googleApi = googleApi;
-    this.googleAuth = googleAuth;
   }
 
   ngOnInit(): void {
@@ -23,7 +21,7 @@ export class AuthModalComponent implements OnInit {
 
   googleLogin() {
     console.log("google login");
-    console.log(this.googleAuth.getGoogleAuthUrl());
+    this.googleApi.authenticate("button");
   }
 
 
